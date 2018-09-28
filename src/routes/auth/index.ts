f771a09faab86b2907b4f1e32405773d9efe657b
godsenal <tmqps78@gmail.com>
2018-09-28 16:46:53 +0900

    Add login with kakao

--- src/routes/auth/index.ts
+++ src/routes/auth/index.ts
@@ -5,19 +5,23 @@ import * as controller from './controller';
 
 const router = express.Router();
 
+// oauth routing
+// api 한번 정리 필요!
 router.get(
   '/oauth/google',
   passport.authenticate('google', {
     scope: ['profile', 'email'],
   }),
 );
-router.put('/oauth/profile', controller.putProfile);
-// router.get('/kakao', controller.kakaoLogin);
 router.get(
   '/oauth/callback',
   passport.authenticate('google', { failureRedirect: '/login' }),
   controller.callback,
 );
+router.put('/oauth/profile', controller.putProfile);
+// Kakao 로그인 테스트
+router.get('/oauth/kakao', controller.oauthKakao);
+router.get('/oauth/callback/kakao', controller.callbackKakao);
 
 router.get('/profile', controller.getProfile);
 router.get('/logout', controller.logout);
